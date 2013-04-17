1494b1c0f9875757a6713082fbb7cff5a7e2a52e
Valeriy Zinchenko <valllllera@gmail.com>
2013-04-17 12:02:31 +0300

    ***

---
+++ CookItNow/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CookItNow
+//
+//  Created by Valeriy Zinchenko on 26.12.12.
+//  Copyright (c) 2012 vexadev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
