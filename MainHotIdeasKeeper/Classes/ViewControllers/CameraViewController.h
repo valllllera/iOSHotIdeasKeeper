290fb0ce8c202b091842c8fdf419c3d03184d527
sanyaslinkz <alexkol29@mail.ru>
2013-04-18 15:33:59 +0200

    new files

---
+++ MainHotIdeasKeeper/Classes/ViewControllers/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  MainHotIdeasKeeper
+//
+//  Created by Alexandr Kolesnik on 18.04.13.
+//  Copyright (c) 2013 vexadev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end
---
+++ MainHotIdeasKeeper/Classes/ViewControllers/CameraViewController.m
@@ -0,0 +1,38 @@
+//
+//  CameraViewController.m
+//  MainHotIdeasKeeper
+//
+//  Created by Alexandr Kolesnik on 18.04.13.
+//  Copyright (c) 2013 vexadev. All rights reserved.
+//
+
+#import "CameraViewController.h"
+
+@interface CameraViewController ()
+
+@end
+
+@implementation CameraViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
